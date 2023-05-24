b859bdecd708bbb8f89645bcff44904bb1a83d77
pruthviraj9696 <kohpruthviraj9696@gmail.com>
2023-05-24 10:57:32 +0530

    whether

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react';
 function App() {
+
+const [lat,setLat] =useState(0)
+const [long,setLong] =useState(0)
+const [hemisphere,setHemisphere] =useState("")
+const [month,setMonth] =useState("")
+
+function getCurrentLocation(){
+  navigator.geolocation.getCurrentPosition((position)=>{
+    let {latitude,longitude}=position.coords
+    setLat (latitude)
+    setLong(longitude)
+    getHemisphere(latitude)
+  })
+
+  function getHemisphere(latitude){
+    if(latitude<0){
+      setHemisphere('Northern')
+    }
+
+    else if(latitude>0){
+      setHemisphere('Southern')
+    }
+
+    else{
+      setHemisphere('Equator')
+    }
+    let date =new Date()
+    let currentMonth = date.getMonth()
+    setMonth(currentMonth)
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <h1>latitude : {lat}  longitude:{long}</h1>
+         <h1>hemisphere: {hemisphere}</h1> 
+         <h1>month: {month}</h1>  
+         <button onClick={getCurrentLocation}>Get Current Location</button>  
     </div>
   );
 }
